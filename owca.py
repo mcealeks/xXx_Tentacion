5c60214eb1ba9b87810aa51a73bce26ff5f810cd
qwert@gmail.com <qwerty>
2018-10-11 17:48:33 +0200

    dodano zadanie domowe

--- owca.py
+++ owca.py
@@ -1,1 +1,27 @@
 ileOwiec = input("jak duza piramide z owiec mam narysowac: ")
+print("przed konwertowanie to jest typ: ",
+      type(ileOwiec))
+ileOwiec = int(ileOwiec)
+print("Po konwertowaniu to jest typ: ",
+      type(ileOwiec)
+
+#Zadanie Domowe
+"""
+
+    Napisać program który pobierze liczbę od urzytkownika
+    i wyśiewietli piramide z kotów lub owiec.
+    Przykład:
+    Dla 3 liczby 3 i załóżmy kotów =^.^=:
+
+    =^.^=
+    =^.^==^.^=
+    =^.^==^.^==^.^=
+    Hint: ify i fory
+    Zadanie dodatkowe 1.5 pkt
+    Zabezpieczyć się przed errorem który wyskakuje gdy user wpisze do inputa,
+    napis a nie liczbe
+
+"""
+
+
+    
